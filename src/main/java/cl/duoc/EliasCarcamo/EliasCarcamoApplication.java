dcbae209b6840a53d582173cca71a07ad251e605
EliasCarcamo18 <el.carcamo@duocuc.cl>
2026-03-31 10:19:30 -0300

    guardar proyecto

---
+++ src/main/java/cl/duoc/EliasCarcamo/EliasCarcamoApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.EliasCarcamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliasCarcamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliasCarcamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/EliasCarcamo/EliasCarcamoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.EliasCarcamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliasCarcamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
